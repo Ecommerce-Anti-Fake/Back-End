f672e2375954137d88b705cba3cca883cbf8d4a4
TDrake11 <tantqse172039@fpt.edu.vn>
2026-06-11 23:34:18 +0700

    Fix login V3

--- apps/api-gateway/src/bootstrap-http.ts
+++ apps/api-gateway/src/bootstrap-http.ts
@@ -8,6 +8,8 @@ const DEFAULT_ALLOWED_ORIGINS = [
   'https://antifake.io.vn',
   'https://www.antifake.io.vn',
   'https://api.antifake.io.vn/api',
+  'https://ecommerce-anti-fake-back-end.onrender.com/',
+  'https://ecommerce-anti-fake-back-end.onrender.com/api',
 ];
 
 function parseOriginList(value?: string) {
