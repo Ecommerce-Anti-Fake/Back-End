de6c2326ab842cec39a38992bc0aed4033f24d92
TDrake11 <tantqse172039@fpt.edu.vn>
2026-06-11 22:46:53 +0700

    Fix error DashboardController -> JwtService.

--- apps/api-gateway/src/modules/dashboard/dashboard.module.ts
+++ apps/api-gateway/src/modules/dashboard/dashboard.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthGuardsModule } from '@security';
 import { GatewayUserModule } from '../user/user.module';
 import { DashboardController } from './dashboard.controller';
 
 @Module({
-  imports: [GatewayUserModule],
+  imports: [AuthGuardsModule, GatewayUserModule],
   controllers: [DashboardController],
 })
 export class GatewayDashboardModule {}
